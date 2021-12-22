e46950d7af8883264e0ca768baccea3854b52b94
Paul Tunison <paul.tunison@kitware.com>
2021-12-22 12:07:14 -0500

    Expose all interfaces at the module root level

--- docs/release_notes/pending_release.rst
+++ docs/release_notes/pending_release.rst
@@ -13,5 +13,8 @@ Features
   Source for the model used was taken from:
   https://github.com/GNAYUOHZ/centernet-visdrone
 
+* Moved, and added to, interface convenience exposure to the package root
+  module.
+
 Fixes
 -----
---
+++ smqtk_detection/__init__.py
@@ -0,0 +1,6 @@
+from smqtk_detection._defaults import DFLT_DETECTION_FACTORY  # noqa: F401
+from smqtk_detection.interfaces.detect_image_objects import DetectImageObjects  # noqa: F401
+from smqtk_detection.interfaces.detection_element import DetectionElement  # noqa: F401
+from smqtk_detection.interfaces.object_detector import (  # noqa: F401
+    ObjectDetector, ImageMatrixObjectDetector
+)
